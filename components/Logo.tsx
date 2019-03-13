c3c32e8cc0c99a1c62d72531c906bed84140a4fc
Edwin Cloud <ecloud412@gmail.com>
2019-03-13 01:02:17 -0700

    Style adjustments

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,8 +4,11 @@ export default styled.img`
   width: 180px;
   height: auto;
   object-fit: contain;
-  margin: 30px 30px;
+  margin: 30px 0 70px 30px;
   @media (max-width: 768px) {
       justify-self: center;
+      margin-left: 0;
+      margin-bottom: 30px;
   }
+
 `;
