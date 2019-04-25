cb7c99a0a80e642af2937daf962c89b12ebee58d
Edwin Cloud <ecloud412@gmail.com>
2019-04-25 13:39:44 -0700

    Started working on ViewRecipeModal

--- components/RecipePreviewTile.tsx
+++ components/RecipePreviewTile.tsx
@@ -1,5 +1,6 @@
-import { styled, Image, Grid, Heading } from 'reakit';
+import { styled, Image, Grid, Heading, Overlay, Portal, Backdrop } from 'reakit';
 import { NextStatelessComponent } from 'next';
+import { ViewRecipeModal } from './';
 import React from 'react';
 
 type Difficulty = 'easy' | 'mid' | 'hard';
@@ -15,28 +16,38 @@ interface Props extends DifficultyFabProps {
   price: string | number;
 }
 
-const RecipePreviewTile: NextStatelessComponent<Props> = (props: Props): any => (
-  <Tile>
-    <Picture>
-      <source srcSet={(props.hasOwnProperty('images') && props.images.webp) || ''} type="image/webp" />
-      <source srcSet={(props.hasOwnProperty('images') && props.images.jpg) || ''} type="image/jpeg" />
-      <Image
-        src={(props.hasOwnProperty('images') && props.images.jpg) || ''}
-        alt={props.name.toLowerCase()}
-        objectFit="contain"
-      />
-    </Picture>
-    <DifficultyFab difficulty={props.difficulty}>{props.difficulty.toUpperCase()}</DifficultyFab>
-    <Heading use="h3" color="#727272" fontSize={22}>
-      COOK TIME:{' '}
-      {(isNaN(Number(props.time)) && `n/a`) ||
-        ((Number(props.time) == 1 && `${props.time} HOUR`) || `${props.time} HOURS`)}
-      {props.price && ` \u00B7 $${props.price}/PERSON`}
-    </Heading>
-    <Heading use="h2" color="#4a4a4a" marginTop={0} fontSize={40}>
-      {props.name}
-    </Heading>
-  </Tile>
+const RecipePreviewTile: NextStatelessComponent<Props> = (props: Props): JSX.Element => (
+  <Overlay.Container>
+    {(overlay: any): JSX.Element => (
+      <>
+        <Tile onClick={overlay.show}>
+          <Picture>
+            <source srcSet={(props.hasOwnProperty('images') && props.images.webp) || ''} type="image/webp" />
+            <source srcSet={(props.hasOwnProperty('images') && props.images.jpg) || ''} type="image/jpeg" />
+            <Image
+              src={(props.hasOwnProperty('images') && props.images.jpg) || ''}
+              alt={props.name.toLowerCase()}
+              objectFit="contain"
+            />
+          </Picture>
+          <DifficultyFab difficulty={props.difficulty}>{props.difficulty.toUpperCase()}</DifficultyFab>
+          <Heading use="h3" color="#727272" fontSize={18} fontWeight={700}>
+            COOK TIME:{' '}
+            {(isNaN(Number(props.time)) && `n/a`) ||
+              ((Number(props.time) == 1 && `${props.time} HOUR`) || `${props.time} HOURS`)}
+            {props.price && ` \u00B7 $${props.price}/PERSON`}
+          </Heading>
+          <Heading use="h2" color="#4a4a4a" marginTop={0} fontSize={34} fontWeight={700}>
+            {props.name}
+          </Heading>
+        </Tile>
+        <Backdrop use={[Portal, Overlay.Hide]} {...overlay} />
+        <Overlay use={Portal} {...overlay}>
+          <ViewRecipeModal overlay={overlay} {...props} />
+        </Overlay>
+      </>
+    )}
+  </Overlay.Container>
 );
 
 export default RecipePreviewTile;
@@ -45,6 +56,12 @@ export default RecipePreviewTile;
 const Tile = styled(Grid)`
   position: relative;
   margin: 40px;
+  :hover {
+    cursor: pointer;
+    h2 {
+      text-decoration: underline;
+    }
+  }
 `;
 const Picture = styled.picture`
   z-index: -1;
@@ -56,37 +73,19 @@ const DifficultyFab = styled.div<DifficultyFabProps>`
   position: absolute;
   z-index: 1;
   right: 0;
-  width: 83px;
-  height: 83px;
+  width: 63px;
+  height: 63px;
   border-radius: 50%;
-  border: 7px solid white;
+  border: 5px solid white;
   line-height: 3.6;
   text-align: center;
-  font-size: 24px;
+  font-size: 19px;
   color: #fff;
-  margin-top: -41px;
-  margin-right: -41px;
-  ${props =>
+  margin-top: -31px;
+  margin-right: -31px;
+  ${(props: Props): string =>
     props.difficulty &&
     ((props.difficulty === 'easy' && `background-color: #a9bfc6;`) ||
       (props.difficulty === 'mid' && `background-color: #698e9a;`) ||
       (props.difficulty === 'hard' && `background-color: #295e70;`))}
 `;
-// const Title = styled.div`
-//   position: absolute;
-//   background-color: rgba(0, 0, 0, 0.2);
-//   top: 0;
-//   left: 0;
-//   width: 100%;
-//   height: 100%;
-//   h2 {
-//     position: relative;
-//     margin: 0 10px;
-//     top: 50%;
-//     transform: translateY(-50%);
-//     font-size: 48px;
-//     font-weight: 600;
-//     color: #fff;
-//     text-align: center;
-//   }
-// `;
---
+++ components/ViewRecipeModal.tsx
@@ -0,0 +1,80 @@
+import { styled, Grid, Heading, Paragraph, Image } from 'reakit';
+import { NextStatelessComponent } from 'next';
+import React from 'react';
+
+interface Props {
+  overlay: any;
+}
+
+const ViewRecipeModal: NextStatelessComponent<Props> = (props: Props): JSX.Element => (
+  <Grid width="90vw" height="90vh" position="relative">
+    <CloseButton onClick={props.overlay.hide} />
+    {/* TOP ROW */}
+    <Grid margin={20} templateColumns="1fr 1fr" gap={20}>
+      {/* Left Col */}
+      <Grid height="min-content">
+        <Heading use="h2" color="#042347" margin={0} fontSize={60} fontWeight={700}>
+          {props.name}
+        </Heading>
+        <Heading use="h3" color="#727272" fontSize={22} marginTop={5} marginBottom={20} fontWeight={700}>
+          {`EASY DINNER \u00B7 `}
+          COOK TIME:{' '}
+          {(isNaN(Number(props.time)) && `n/a`) ||
+            ((Number(props.time) == 1 && `${props.time} HOUR`) || `${props.time} HOURS`)}
+          {props.price && ` \u00B7 $${props.price}/PERSON`}
+        </Heading>
+        {/* Reviews Preview */}
+        <Grid>
+          <Heading use="h4" fontWeight={600} color="#4a4a4a" margin={0}>
+            Top Review:
+          </Heading>
+          <Paragraph color="#4a4a4a" fontWeight={300} fontSize={20}>
+            I found out the secret to making these right. First, I had wilted lacinato kale in the fridge that I made
+            stiff by cutting the stems and letting them sit in water overnight, like flowers. Then, I washed them and
+            made sure they were very dry. (Turned on the oven at 350.) I de-stemmed the kale and cut them into the size
+            of the bigger U.S…
+          </Paragraph>
+        </Grid>
+      </Grid>
+      <Picture>
+        <source srcSet={(props.hasOwnProperty('images') && props.images.webp) || ''} type="image/webp" />
+        <source srcSet={(props.hasOwnProperty('images') && props.images.jpg) || ''} type="image/jpeg" />
+        <Image
+          src={(props.hasOwnProperty('images') && props.images.jpg) || ''}
+          alt={props.name.toLowerCase()}
+          objectFit="contain"
+        />
+      </Picture>
+    </Grid>
+  </Grid>
+);
+
+export default ViewRecipeModal;
+
+// Styled Components and Media Queries
+const CloseButton = styled.div`
+  :before {
+    content: 'x';
+  }
+  :hover {
+    background: rgba(234, 234, 234, 0.4);
+  }
+  position: absolute;
+  right: 0;
+  border-radius: 6px;
+  font-size: 32px;
+  font-weight: 900;
+  color: #444;
+  padding: 0 8px;
+  line-height: 1;
+  height: 35px;
+`;
+
+const Picture = styled.picture`
+  z-index: -1;
+  img {
+    width: 100%;
+    max-height: 60vh;
+  }
+  margin: 30px;
+`;
